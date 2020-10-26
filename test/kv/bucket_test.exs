654ed7b4cf8479488361ecedaff822379758369c
Michael Johnston <lastobelus@mac.com>
2020-10-26 15:30:50 -0700

    test for kv/bucket

---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,14 @@
+defmodule KV.BucketTest do
+  @moduledoc """
+  Tests for KV.Bucket
+  """
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
