4810817c211a24a45049600ff1a4ba2bb1163d4d
Michael Johnston <lastobelus@mac.com>
2020-11-02 11:22:14 -0800

    fix: launch KV.BucketSupervisor first
    KV.Registry refers to KV.BucketSupervisor, so it
    must be launched after KV.BucketSupervisor.
    
    Also, this reference means a restarted KV.BucketSupervisor
    would mean a bad reference in KV.Registry,
    so we change the supervision strategy to :one_for_all.
    If any child crashes, all will be restarted in correct
    order to properly link them.

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -3,6 +3,7 @@ defmodule KV.Supervisor do
   Supervisor for the bucket Registry.
   """
   use Supervisor
+
   @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
   @doc """
     Starts the KV Supervisor
@@ -19,17 +20,21 @@ defmodule KV.Supervisor do
   """
   def init(:ok) do
     children = [
-      {KV.Registry, name: KV.Registry},
       # Using a DynamicSupervisor for KV.Bucket will ensure that if a bucket crashes,
       # it does not crash the registry
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      {KV.Registry, name: KV.Registry}
     ]
 
-    # one-for-one means that if a child dies, it will be the only one restarted
+    # ~~one-for-one means that if a child dies, it will be the only one restarted
     # Supervisor init will traverse the list of children invoking child_spec/1
     # GenServer, Agent, Supervisor etc. all define a default child_spec/1
-    # Then each child is started in order, using the start: defined in its child_spec
-    Supervisor.init(children, strategy: :one_for_one)
+    # Then each child is started in order, using the start: defined in its child_spec~~
+    #
+    # one-for-all means that if a child dies, all children will be restarted, in the
+    # defined order. Now that we are using a DynamicSupervisor for buckets, this is
+    # the more suitable option.
+    Supervisor.init(children, strategy: :one_for_all)
   end
 
   # endregion [ callbacks ]
